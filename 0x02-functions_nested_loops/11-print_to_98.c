95138b208a4764911f329bf6356c1cf612d9cc43
JaymzMk <Jamesmwaikmt@gmail.com>
2022-06-07 15:50:10 -0700

    Tasks

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers to 98
+ * @a: starting integer
+ * Description: prints all natural number from n - 98
+ *
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+	if (a < 98)
+	{
+		while (a < 98)
+		{
+			printf("%d, ", a);
+			a++;
+		}
+	}
+	else if (a > 98)
+	{
+		while (a > 98)
+		{
+			printf("%d, ", a);
+			a--;
+		}
+	}
+	printf("98\n");
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int cycle = 0;
+	char alph = 'a';
+
+	while (cycle < 10)
+	{
+		alph = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(alph);
+			alph++;
+		}
+		_putchar('\n');
+
+		cycle++;
+	}
+}
