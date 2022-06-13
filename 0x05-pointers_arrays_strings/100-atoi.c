9d939458e369327dfd811a9e3726b5cd44ae15a2
JaymzMk <Jamesmwaikmt@gmail.com>
2022-06-13 13:51:46 -0700

    Corrections-Advance

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,23 +1,33 @@
 #include "main.h"
-
 /**
- * main - generates keygen.
- * Return: 0 Always.
+ * _atoi - converts a string to an integer.
+ * @s: the string to convert
+ *
+ * Return: the converted string.
  */
-int main(void)
+int _atoi(char *s)
 {
-	int r = 0, c = 0;
-	time_t t;
+	short boolean;
+	int i, minus, result;
 
-	srand((unsigned int) time(&t));
-	while (c < 2772)
+	i = minus = result = boolean = 0;
+	minus = -1;
+
+	while (s[i] != '\0')
 	{
-		r = rand() % 128;
-		if ((c + r) > 2772)
+		if (s[i] == '-')
+			minus *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result *= 10;
+			result -= (s[i] - '0');
+			boolean = 1;
+		}
+		else if (boolean == 1)
 			break;
-		c = c + r;
-		printf("%c", r);
+		i++;
 	}
-	printf("%c\n", (2772 - c));
-	return (0);
+	result *= minus;
+	return (result);
 }
