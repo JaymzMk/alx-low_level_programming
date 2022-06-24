51dfe04aa5211aac997f3500f63940ecd36daa6e
JaymzMk <Jamesmwaikmt@gmail.com>
2022-06-24 06:10:28 -0700

    Advance tasks corrections

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,38 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints the minimum number of coins to make change for an amount.
- * of money.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
- */
+ * main - prints the minimum number of coins to make change for a given amount
+ * @argc: arguement count
+ * @argv: array of pointers to arguement strings
+ * Return: number of coins or 1
+ **/
 int main(int argc, char *argv[])
 {
-	int cents, acoins = 0;
+	int amt, coins;
 
-	if (argc == 1 || argc > 2)
+	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	cents = atoi(argv[1]);
-
-	while (cents > 0)
+	amt = atoi(argv[1]);
+	coins = 0;
+	if (amt > 25)
 	{
-		if (cents >= 25)
-			cents -= 25;
-		else if (cents >= 10)
-			cents -= 10;
-		else if (cents >= 5)
-			cents -= 5;
-		else if (cents >= 2)
-			cents -= 2;
-		else if (cents >= 1)
-			cents -= 1;
-		ncoins += 1;
+		while (amt >= 25)
+			amt -= 25, coins++;
 	}
-	printf("%d\n", acoins);
+	if (amt > 10 && amt < 25)
+	{
+		while (amt >= 10)
+			amt -= 10, coins++;
+	}
+	if (amt > 5 && amt < 10)
+	{
+		while (amt >= 5)
+			amt -= 5, coins++;
+	}
+	if (amt > 2 && amt < 5)
+	{
+		while (amt >= 2)
+			amt -= 2, coins++;
+	}
+	if (amt == 1 || amt == 2 || amt == 5 || amt == 10 || amt == 25)
+	{
+		coins++;
+	}
+	printf("%d\n", coins);
 	return (0);
 }
